51821f954eb170dd5517054bab2025b478446862
theo.gautier <contact@theogautier.fr>
2026-01-03 17:32:06 +0100

    tech: healthcheck api

---
+++ src/routes/health/+server.ts
@@ -0,0 +1,12 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async () => {
+	return json(
+		{
+			status: 'ok',
+			timestamp: new Date().toISOString()
+		},
+		{ status: 200 }
+	);
+};
