a7b587d431081c1adf4c6b0b6110de0bbf8308a6
theo.gautier <contact@theogautier.fr>
2026-01-22 11:02:13 +0100

    feat: menu navigation

--- src/lib/components/common/Header.svelte
+++ src/lib/components/common/Header.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import {CircleUserIcon, LogOutIcon, MenuIcon, Loader2} from '@lucide/svelte';
-  import {AppBar, ToggleGroup} from '@skeletonlabs/skeleton-svelte';
+  import {ChessQueen, CircleUserIcon, Database, Loader2, LogOutIcon, MenuIcon} from '@lucide/svelte';
+  import {AppBar, Menu, Portal, ToggleGroup} from '@skeletonlabs/skeleton-svelte';
   import type {User} from 'lucia';
   import {_, locale} from '$lib/i18n';
   import {updateUserLanguage} from '$lib/services/languageService';
@@ -28,9 +28,51 @@
 <AppBar class="border-b-1 border-b-primary-100 bg-[#121212]">
     <AppBar.Toolbar class="grid-cols-[auto_1fr_auto]">
         <AppBar.Lead>
-            <button class="btn-icon btn-icon-lg hover:preset-tonal" type="button">
-                <MenuIcon/>
-            </button>
+            <Menu>
+                <Menu.Trigger class="btn-icon btn-icon-lg hover:preset-tonal">
+                    <MenuIcon/>
+                </Menu.Trigger>
+                <Portal>
+                    <Menu.Positioner>
+                        <Menu.Content>
+                            {#if user}
+                                <Menu.Item value="analysis">
+                                    <a class="hover:preset-tonal" href="/chessboard">
+                                        <ChessQueen class="size-4 btn-icon"/>
+                                        {$_('common.nav.chessboard')}
+                                    </a>
+                                </Menu.Item>
+                                <Menu.Item value="database">
+                                    <a class="hover:preset-tonal" href="/database">
+                                        <Database class="size-4 btn-icon"/>
+                                        {$_('common.nav.database')}
+                                    </a>
+                                </Menu.Item>
+                                <Menu.Item value="account">
+                                    <a class="hover:preset-tonal" href="/me">
+                                        <CircleUserIcon class="size-4 btn-icon"/>
+                                        {$_('common.nav.profile')}
+                                    </a>
+                                </Menu.Item>
+                                <Menu.Item value="logout">
+                                    <form method="POST" action="/logout">
+                                        <button class="hover:preset-tonal" type="submit">
+                                            <LogOutIcon class="size-4 btn-icon"/>
+                                            {$_('common.nav.logout')}
+                                        </button>
+                                    </form>
+                                </Menu.Item>
+                            {:else}
+                                <Menu.Item value="login">
+                                    <a class="btn preset-filled-primary-500" href="/login">
+                                        {$_('auth.login.submit')}
+                                    </a>
+                                </Menu.Item>
+                            {/if}
+                        </Menu.Content>
+                    </Menu.Positioner>
+                </Portal>
+            </Menu>
         </AppBar.Lead>
         <AppBar.Headline>
             <a href="/" title="Accueil">
@@ -39,7 +81,7 @@
         </AppBar.Headline>
         <AppBar.Trail>
             <div class="flex items-center gap-2">
-                <ToggleGroup value={currentLocale} onValueChange={handleLanguageChange} disabled={isChangingLanguage}>
+                <ToggleGroup disabled={isChangingLanguage} onValueChange={handleLanguageChange} value={currentLocale}>
                     <ToggleGroup.Item value="FR">
                         FR
                     </ToggleGroup.Item>
@@ -48,7 +90,7 @@
                     </ToggleGroup.Item>
                 </ToggleGroup>
                 {#if isChangingLanguage}
-                    <Loader2 class="size-5 animate-spin text-primary-500" />
+                    <Loader2 class="size-5 animate-spin text-primary-500"/>
                 {/if}
             </div>
 
